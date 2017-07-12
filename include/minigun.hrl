644bfa27f7fa707344e51f11a93ffe2a8019e3c9
Byron Wang <byronpc1@gmail.com>
2017-07-12 11:43:18 +0800

    Create monotonic positive ids for pool ids

--- include/minigun.hrl
+++ include/minigun.hrl
@@ -1,3 +1,5 @@
 -define(SUPERVISOR(PoolId), list_to_atom(atom_to_list(PoolId) ++ "_sup")).
 -define(GENERATE_WORKER_ID(PoolId), list_to_atom(
-  atom_to_list(PoolId) ++ "_" ++ integer_to_list(erlang:system_time(microsecond)))).
+  atom_to_list(PoolId) ++ "_" ++
+  integer_to_list(erlang:unique_integer([positive, monotonic]))
+)).
